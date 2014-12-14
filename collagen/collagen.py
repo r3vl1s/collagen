730c10535ae82a3c8a2a59f3d984be89da3e936b
r3vl1s <timothy.b.seifert@gmail.com>
2014-12-13 20:57:52 -0600

    generate different canvases

--- collagen/collagen.py
+++ collagen/collagen.py
@@ -29,18 +29,17 @@ def collagen(source_folder, output_folder):
     config = utils.load_config('config')
 
     num_collages = config[':collage'][':number_of_collages']
-    max_pastes = config[':collage'][':number_of_pastes']
+    max_pastes = config[':collage'][':max_pastes']
+    min_pastes = config[':collage'][':min_pastes']
     segments_per_image = config[':segments_per_image'] 
-    collage_width = config[':collage'][':width']
-    collage_height = config[':collage'][':height']
+    image_dim = (config[':collage'][':width'], config[':collage'][':height'])
     segment_path = os.path.join(COLLAGEN_DIR, config[':segments_folder'])
-    
 
     segment_images(source_folder, segment_path, segments_per_image, config)
 
     name = []
     for i in range(num_collages):
-        name.append(collage.collage(segment_path, output_folder, max_pastes, collage_width, collage_height))
+        name.append(collage.collage(segment_path, output_folder, min_pastes, max_pastes, image_dim))
 
     utils.delete_folder_files(segment_path)
 
